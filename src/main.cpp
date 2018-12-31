b8d8bb7f53de09fe62d6eb87b52219c14c24e51a
Pascal Baltazar <pascal@baltazars.org>
2018-12-31 12:55:36 +0100

    make the app fullscreen

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main()
   settings.width = 1280;
   settings.height = 960;
   settings.title = "nebula-eye";
+  settings.windowMode = OF_FULLSCREEN;
   auto window = ofCreateWindow(settings);
   auto app = make_shared<nebulaEye>();
   ofRunApp(window, app);
